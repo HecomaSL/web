d82cc6ecc3f1ca4935f6a4f9cdfc34402712f517
Marketing HECOMA <marketing@hecoma.com>
2026-04-23 09:27:56 +0200

    Fallo ruta crear cuenta

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -52,6 +52,6 @@ class RegisteredUserController extends Controller {
 
         Auth::login($user);
 
-        return redirect(route('/', absolute: false));
+        return redirect(route('/Dashboard', absolute: false));
     }
 }
