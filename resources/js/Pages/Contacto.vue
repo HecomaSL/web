6986d35e960cebc4de2a4dfd6b95dba5d7bad665
Marketing HECOMA <marketing@hecoma.com>
2026-04-27 16:55:45 +0200

    slider inicio + horas en contacto

--- resources/js/Pages/Contacto.vue
+++ resources/js/Pages/Contacto.vue
@@ -25,26 +25,28 @@ const enviarFormulario = () => {
         <section class="container mx-auto px-6">
             <div class="max-w-6xl mx-auto space-y-6 text-gray-700 leading-relaxed text-lg">
                 <p>Sabemos que en el mundo del mecanizado de la madera, cada detalle cuenta. Por eso, en HECOMA, queremos estar siempre cerca de tí para asesorarte de la manera que te resulte más cómoda.</p>
-                <p>Puedes contactarnos de la manera que te resulte más cómoda: vía email, teléfono, whatsapp, en nuestras oficinas o a través de nuestro formulario de contacto. También nos puedes encontrar en nuestras redes sociales para estar al día de nuestras novedades. Nuestro equipo estará encantado de ayudarte y resolver cualquier duda que tengas.</p>
+                <p>Puedes contactarnos de la manera que te resulte más cómoda: vía <strong> <a href="mailto:dptocomercial@hecoma.com" title="Correo de contacto">email</a></strong>, <strong><a href="tel:+34 667 40 76 92">teléfono</a></strong>, <strong><a href="https://api.whatsapp.com/send?phone=34667407692&amp;text=Hola!%20Quiero%20informarme%20sobre%20sus%20productos">whatsapp</a></strong>, en <strong><a href="#sedes">nuestras oficinas</a></strong> o a través de nuestro <a href="#formulario-contacto"><strong>formulario de contacto</strong></a>. También nos puedes encontrar en nuestras redes sociales para estar al día de nuestras novedades. Nuestro equipo estará encantado de ayudarte y resolver cualquier duda que tengas.</p>
             </div>
         </section>
 
-        <section class=" py-16 px-6">
+        <section id="sedes" class=" py-16 px-6">
             <div class="container mx-auto max-w-6xl">
                 <div class="text-center mb-12">
                     <h2 class="text-3xl md:text-4xl font-bold leading-tight uppercase tracking-wide mb-4">SEDES</h2>
                 </div>
-
                 <div class="flex flex-col md:flex-row gap-8 justify-center items-stretch">
                     <div class="flex-1 bg-white border border-gray-100 rounded-xl shadow-md p-4 flex flex-col items-center">
                         <h3 class="text-[#003399] text-xl md:text-2xl font-bold uppercase mb-4">Sonseca</h3>
-                        <div class="w-full h-[350px] overflow-hidden rounded-lg">
+                        <p>De Lunes a Viernes de 9.00 - 14.00 y 16.00 - 18.00</p>
+                        <div class="w-full h-[350px] overflow-hidden rounded-lg mt-7">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.42047632631!2d-3.982636!3d39.685249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6a115e701d7ea7%3A0x778ccf570a392130!2sHerramientas%20de%20corte%20para%20madera%20S.L.%20HECOMA!5e0!3m2!1ses!2ses!4v1776407987341!5m2!1ses!2ses" class="w-full h-full" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
 
                     <div class="flex-1 bg-white border border-gray-100 rounded-xl shadow-md p-4 flex flex-col items-center">
                         <h3 class="text-[#003399] text-xl md:text-2xl font-bold uppercase mb-4">Lucena</h3>
+                        <p>De Lunes a Jueves de 8.00 a 14.00 y 16.00 - 18.00</p>
+                        <p>Viernes de 7.00 - 15.00</p>
                         <div class="w-full h-[350px] overflow-hidden rounded-lg">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.7251254704947!2d-4.5082153!3d37.3963323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6d70d203160d91%3A0x858d2135793072e7!2sHECOMA%20%26%20Incoypre!5e0!3m2!1ses!2ses!4v1776408039362!5m2!1ses!2ses" class="w-full h-full" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
@@ -52,7 +54,7 @@ const enviarFormulario = () => {
                 </div>
             </div>
         </section>
-        <section class="py-16 px-6 bg-[#f5f9fb]">
+        <section id="formulario-contacto" class="py-16 px-6 bg-[#f5f9fb]">
             <div class="text-center mb-12">
                 <h2 class="text-3xl md:text-4xl font-bold leading-tight uppercase tracking-wide mb-4">FORMULARIO DE CONTACTO</h2>
             </div>
@@ -96,8 +98,12 @@ const enviarFormulario = () => {
 </template>
 
 <style scoped>
-  /* Ajustes para imitar el interlineado y estilo de la imagen corporativa */
+  :global(html) { scroll-behavior:smooth; }
+
+  section { scroll-margin-top: 50px; }
+
   p { text-align: justify; line-height: 1.6; }
   h2 { line-height: 1.2; }
   h3 { line-height: 1.2; }
+  a { color:blue; }
 </style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,12 +1,10 @@
 <script setup>
 import { computed } from 'vue';
-import { usePage, Link, Head } from '@inertiajs/vue3'; // <--- FALTA IMPORTAR Head
+import { usePage, Link, Head } from '@inertiajs/vue3';
 import MainLayout from '@/Layouts/MainLayout.vue';
 
-// Accedemos a las propiedades compartidas de Inertia
 const page = usePage();
 
-// Creamos una propiedad computada para saber el destino del enlace
 const areaTecnicaLink = computed(() => {
     return page.props.auth.user ? '/dashboard' : '/login';
 });
@@ -18,52 +16,76 @@ const areaTecnicaLink = computed(() => {
         <section class="sectionColor bg-[#0000BB] text-white py-16 px-6">
             <div class="container mx-auto max-w-5xl text-center">
                 <div class="mb-8 flex justify-center">
-                    <img src="https://hecoma.com/wp-content/uploads/2020/12/lgoo-hecoma-blanco.png" alt="HECOMA Logo" class="h-24 md:h-32 object-contain" />
+                    <img src="/images/logo-hecoma-blanco.png" alt="HECOMA Logo" class="h-24 md:h-32 object-contain" />
                 </div>
                 <h1 class="text-3xl md:text-4xl font-bold mb-8 leading-tight uppercase tracking-wide">Fabricación de Herramientas de Corte para Madera de Alto Rendimiento</h1>
-                <div class="text-lg md:text-xl leading-relaxed max-w-4xl mx-auto space-y-6">
-                    <p class="text-justify">Como fabricantes de herramientas de corte para madera, madera técnica y plásticos, ofrecemos soluciones en diamante (PCD) y metal duro (HM). Con plantas de producción en Sonseca (Toledo) y Lucena (Córdoba), cubrimos el centro y sur de España con asesoramiento técnico presencial. Especialistas en el diseño de herramientas especiales a medida con plazos de entrega mínimos para optimizar tu producción industrial.</p>
+                <p class="text-xl text-center">Como fabricantes de herramientas de corte para madera, madera técnica y plásticos, ofrecemos soluciones en diamante (PCD) y metal duro (HM). Con plantas de producción en <a href="https://maps.app.goo.gl/iFUmCyCxncteymou5">Sonseca (Toledo)</a> y <a href="https://maps.app.goo.gl/wXWTaA6HyoetsaGn9">Lucena (Córdoba)</a>, cubrimos el centro y sur de España con asesoramiento técnico presencial. Especialistas en el diseño de herramientas especiales a medida con plazos de entrega mínimos para optimizar tu producción industrial.</p>
+            </div>
+        </section>
+
+        <section class="mt-5 w-full overflow-hidden">
+            <div class="slider-container">
+                <div class="slider" id="slider">
+                    <ul class="slides">
+                        <li class="slide">
+                            <a href="#"><img src="/images/SLIDER1.jpg" alt="SLIDER 1"></a>
+                        </li>
+                        <li class="slide">
+                            <a href="#"><img src="/images/SLIDER2.jpg" alt="SLIDER 2"></a>
+                        </li>
+                        <li class="slide">
+                            <a href="#"><img src="/images/SLIDER3.jpg" alt="SLIDER 3"></a>
+                        </li>
+                        <li class="slide">
+                            <a href="#"><img src="/images/SLIDER4.jpg" alt="SLIDER 4"></a>
+                        </li>
+                        <li class="slide">
+                            <a href="#"><img src="/images/SLIDER1.jpg" alt="SLIDER 1"></a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
 
+        
+
         <section class="py-16 px-6">
             <div class="container mx-auto max-w-5xl text-center">
                 <h2 class="text-3xl md:text-4xl font-bold mb-8 leading-tight uppercase tracking-wide">CENTRO DE RECURSOS Y HERRAMIENTAS DE CORTE DE MADERA</h2>
-                
+
                 <div class="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-6 imgytext-main">
                     <a href="/servicios" class="flex-1">
                         <div class="servicios">
                             <h3>SERVICIOS</h3>
-                            <img src="https://hecoma.com/wp-content/uploads/2020/11/servicios.png" alt="Fresa reparada" width="100%">
+                            <img src="/images/servicios.png" alt="Fresa reparada" width="100%">
                             <p>Nuestra gran diferencia: el desarrollo de herramientas a medida y afilado de calidad</p>
                         </div>
                     </a>
                     <a href="/catalogo" class="flex-1">
                         <div class="catalogo">
                             <h3>CATÁLOGO</h3>
-                            <img src="https://hecoma.com/wp-content/uploads/2020/11/catalogo.png" alt="Fresa reparada" width="100%">
+                            <img src="/images/catalogo.png" alt="Fresa reparada" width="100%">
                             <p>Eche un vistazo a nuestra gran gama de productos para trabajos de madera.</p>
                         </div>
                     </a>
                     <a href="/noticias" class="flex-1">
                         <div class="noticias">
                             <h3>NOTICIAS</h3>
-                            <img src="https://hecoma.com/wp-content/uploads/2020/11/noticias.png" alt="Fresa reparada" width="100%">
-                            <p>Póngase al día con las novedades y noticias sobre el mundo de la herramienta, eventos…</p>
+                            <img src="/images/noticias.png" alt="Fresa reparada" width="100%">
+                            <p>Póngase al día con las novedades y noticias sobre el mundo de la herramienta, eventos… </p>
                         </div>
                     </a>
                     <Link :href="areaTecnicaLink" class="flex-1">
                         <div class="areaTecnica">
                             <h3>AREA TÉCNICA</h3>
-                            <img src="https://hecoma.com/wp-content/uploads/2026/02/Captura-de-pantalla-2026-02-13-121317-1.png" alt="Fresa reparada" width="100%">
-                            <p>Acceda a su cuenta para gestionar sus pedidos, consultar precios y realizar sus compras</p>
+                            <img src="/images/areatecnica.png" alt="Fresa reparada" width="100%">
+                            <p>Acceda a su cuenta para gestionar sus pedidos, consultar precios y realizar sus compras </p>
                         </div>
                     </Link>
                 </div>
             </div>
         </section>
-        
+
         <section class="contacto py-16 px-6 bg-[#f5f9fb]">
             <div class="container mx-auto max-w-6xl">
                 <div class="text-center mb-12">
@@ -88,29 +110,49 @@ const areaTecnicaLink = computed(() => {
                 </div>
             </div>
 
-            <div class="imag mt-12">
-                <img src="/images/financiacion_ue.png" alt="Financiación UE">
+            <div class="imag mt-12 mb-8">
+                <img src="/images/financiacion_ue.png" alt="Financiación UE" class="max-w-full h-auto">
             </div>
 
-            <div class="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-6 imgytext-main">
-                <img src="/images/ayuda1.jpg" alt="Ayuda 1">
-                <img src="/images/ayuda2.png" alt="Ayuda 2">
-                <img src="/images/ayuda3.jpg" alt="Ayuda 3">
-                <img src="/images/ayuda4.jpg" alt="Ayuda 4">
+            <div class="flex flex-wrap justify-center items-center gap-6">
+                <img src="/images/ayuda1.jpg" alt="Ayuda 1" class="h-20 object-contain">
+                <img src="/images/ayuda2.png" alt="Ayuda 2" class="h-20 object-contain">
+                <img src="/images/ayuda3.jpg" alt="Ayuda 3" class="h-20 object-contain">
+                <img src="/images/ayuda4.jpg" alt="Ayuda 4" class="h-20 object-contain">
             </div>
         </section>
     </MainLayout>
 </template>
 
 <style scoped>
-    .sectionColor { background-color: #0000BB; }
-    strong { letter-spacing: 0.02em; }
-    h2, p { -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; }
-    .imgytext-main div { align-items: center; display: flex; flex-direction: column; padding: 1rem; }
-    .imgytext-main p { text-align: center; }
-    .imgytext-main img { height: 200px; width: 200px; object-fit: contain; }
-    .imgytext img { height: auto; max-width: 200px; }
-    .contacto .imgytext { display: flex; align-items: center; }
-    .contacto a { color:blue; }
-    .imag { display: grid; place-items: center; }
+strong { letter-spacing: 0.02em; }
+h2, p { -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; }
+.sectionColor a { text-decoration: underline; font-weight: bold; }
+.sectionColor { background-color: #0000BB; }
+.imgytext-main div { align-items: center; display: flex; flex-direction: column; padding: 1rem; }
+.imgytext-main p { text-align: center; }
+.imgytext-main img { height: 200px; width: 200px; object-fit: contain; }
+.contacto a { color: blue; }
+.imag { display: grid; place-items: center; }
+.slider-container { width: 100%; margin: 0; padding: 0; }
+.slider { width: 100%; position: relative; }
+.slides { display: flex; width: 100%; margin: 0; padding: 0; list-style: none; }
+
+.slide { width: 100vw; flex-shrink: 0; animation: slide-animation 20s linear infinite; }
+
+.slide img { width: 100%; height: auto; display: block; }
+
+.slides:hover .slide { animation-play-state: paused; }
+
+@keyframes slide-animation {
+    0%, 20%   { transform: translateX(0); }
+    25%, 45%  { transform: translateX(-100vw); }
+    50%, 70%  { transform: translateX(-200vw); }
+    75%, 95%  { transform: translateX(-300vw); }
+    100%      { transform: translateX(-400vw); }
+}
+
+* { box-sizing: border-box; }
+
+.contacto img { max-width: 250px; }
 </style>
